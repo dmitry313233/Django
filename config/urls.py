1964b38b14799dad8ba90a12508520da7cd6f506
dmitry313233 <dima.trenirovki@mail.ru>
2023-09-05 09:50:10 +0300

    add commit 3

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', include('catalog.urls', namespace='catalog')),
     path('bloc_record/', include('bloc_record.urls', namespace='bloc_record'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
