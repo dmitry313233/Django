3a5ad5861b05aafa8c6d72c6c90516b44ea2d9df
dmitry313233 <dima.trenirovki@mail.ru>
2023-09-06 09:35:44 +0300

    add commit form

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,15 @@ from django import forms
 from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StileFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'sing_current_version':
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StileFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
@@ -18,13 +26,7 @@ class ProductForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StileFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            if field_name != 'sing_current_version':
-                field.widget.attrs['class'] = 'form-control'
